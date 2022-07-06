a9911f770f4cef8c7f9f954f1d8b1e71332ad69e
AmirHadzhiev <104004036+AmirHadzhiev@users.noreply.github.com>
2022-07-06 19:15:52 +0300

    Add files via upload

---
+++ FunctionalProgramming/KnightsOfHonor.java
@@ -0,0 +1,13 @@
+package Fuctionexercise;
+
+import java.util.Arrays;
+import java.util.Scanner;
+import java.util.function.Consumer;
+
+public class KnightsOfHonor {
+    public static void main(String[] args) {
+         Scanner scanner = new Scanner(System.in);
+        Consumer<String> print = n -> System.out.println("Sir "+n);
+        Arrays.stream(scanner.nextLine().split(" ")).forEach(print);
+    }
+}
