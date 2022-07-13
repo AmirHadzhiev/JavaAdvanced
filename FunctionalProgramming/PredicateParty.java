80beb49c6890808f3d32b748b164e61345dd754a
AmirHadzhiev <104004036+AmirHadzhiev@users.noreply.github.com>
2022-07-13 23:13:59 +0300

    Add files via upload

---
+++ FunctionalProgramming/PredicateParty.java
@@ -0,0 +1,9 @@
+package Fuctionexercise;
+
+import java.util.Scanner;
+
+public class PredicateParty {
+    public static void main(String[] args) {
+         Scanner scanner = new Scanner(System.in);
+    }
+}
